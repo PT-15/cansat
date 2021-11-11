cc963e0c1237c2f5e603b7e21c9c33d5fef9063d
Paula-15 <78314598+Paula-15@users.noreply.github.com>
2021-11-11 10:06:25 +0100

    Create bme280.py

---
+++ bme280.py
@@ -0,0 +1,37 @@
+'''
+Funciones librería
+	 .init (inicializar)
+	 .line (datos)
+	 .writeLogLine (escribir datos a fichero)
+	 .altitud (altitud)
+'''
+
+import board
+import busio
+import adafruit_bme280
+import time
+
+outputLog = None
+bme280 = None
+
+#Inicicaliza el sensor
+def init():
+	global outputLog
+	global bme280
+
+	i2c = busio.I2C(board.SCL, board.SDA)
+	bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+	bme280.sea_level_pressure = 1004.8
+	outputLog = open ('data.txt', 'a')
+
+#Devuelve los datos del sensor
+def line():
+        return "%f %f %f %f %f\n" % (time.time(), bme280.temperature, bme280.humidity, bme280.pressure, bme280.altitude)
+
+#Escribe los datos al archivo de texto
+def writeLogLine():
+        outputLog.write(line())
+        outputLog.flush()
+
+def close():
+	outputLog.close()
