3618ad98f53e00962fc1b0cefba73cae2571d56c
Paula Taibo <paulataibo15@gmail.com>
2021-12-13 13:01:43 +0100

    fix(lora.py): fix wrong variable name in lora library

--- lora.py
+++ lora.py
@@ -16,12 +16,11 @@ from threading import Timer
 def init():
   old_settings = termios.tcgetattr(sys.stdin)
   tty.setcbreak(sys.stdin.fileno())
-  
+
   node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=433,addr=15,power=22,rssi=True,air_speed=2400,relay=False)
 
-  
+
 def mensajePrueba():
   print ("Enviando \"Hola Mundo"")
   mensaje = "Hola Mundo".encode()
-  node.send(data)
-  time.sleep(0.2)
+  node.send(mensaje)
