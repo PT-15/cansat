f2b66595a6a50c701132ebfe131703aeb60e98f5
Paula-15 <paulataibo15@gmail.com>
2022-01-31 13:54:50 +0100

    New lora library

---
+++ loraLibrary.py
@@ -0,0 +1,15 @@
+
+import sys
+import sx126x
+import threading
+import time
+import select
+import termios
+import tty
+from threading import Timer
+
+def init():
+   old_settings = termios.tcgetattr(sys.stdin)
+   tty.setcbreak(sys.stdin.fileno())
+
+   node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=433,addr=0,power=22,rssi=False,air_speed=2400,relay=False)
