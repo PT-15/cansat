5e0bb81cea0df2c41a51862190f6bc9d47b05781
Paula-15 <paulataibo15@gmail.com>
2022-02-08 09:11:47 +0100

    Lora sends all required information

--- loraLibrary.py
+++ loraLibrary.py
@@ -16,8 +16,12 @@ def init():
 
    node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=433,addr=0,power=22,rssi=False,air_speed=2400,relay=False)
 
-def send(num):
-   node.send(num.encode())
+def send(msg):
+   #splitMsg = msg.split()
+   offset_frequence = int(get_t[1])-(850 if int(get_t[1])>850 else 410)
+   #data = bytes([int(get_t[0])>>8]) + bytes([int(get_t[0])&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
+   data = bytes([int(msg) >> 8]) + bytes([int(msg)&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq])
+   node.send(data)
 
 def receive():
    node.receive()
--- sendData.py
+++ sendData.py
@@ -1,17 +1,15 @@
-import bme280
+#import bme280
 import loraLibrary as lora
 from time import sleep
 lora.init()
-bme280.init()
+#bme280.init()
 
 while True:
-   logLine = bme280.line()
-   with open('data.txt', 'at') as log:
-      log.write(logLine)
-      log.flush()
+   #logLine = bme280.line()
+   #with open('data.txt', 'at') as log:
+   #   log.write(logLine)
+   #   log.flush()
 
-   sensorData = logLine.split()
-   for num in sensorData:
-      lora.send(num)
+   lora.send("Hello")
    print ("Line encoded")
-   sleep(1)
+   sleep(2)
